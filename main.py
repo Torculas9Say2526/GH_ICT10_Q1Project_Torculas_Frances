58b1965a63a4059c078deb1276f364a912e53afd
Torculas10Say2627 <torculas.fd@obmontessori.edu.ph>
2026-09-20 18:11:10 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from itertools import product
+from unicodedata import category
+from pyscript import document, display
+
+
+def create_order(e):
+    matcha_ichigo = document.getElementById("item1")
+    dark_matcha = document.getElementById("item2")
+    cold_strawberry_yuzu = document.getElementById("item3")
+    mint_choco_mochi = document.getElementById("item4")
+    white_caramel_frappucino = document.getElementById("item5")
+
+    subtotal = float(matcha_ichigo.value) * float(matcha_ichigo.checked) + float(dark_matcha.value) * float(dark_matcha.checked) + float(cold_strawberry_yuzu.value) * float(cold_strawberry_yuzu.checked) + float(mint_choco_mochi.value) * float(mint_choco_mochi.checked) + float(white_caramel_frappucino.value) * float(white_caramel_frappucino.checked)
+
+    VAT = subtotal * 0.12
+    total = subtotal + VAT
+
+    display(f"Your subtotal is: ₱{subtotal: .2f}", target="show")
+    display(f"Your VAT is: ₱{VAT: .2f}", target="show")
+    display(f"Your total is: ₱{total: .2f}", target="show")
+
+
+def generate_sku(e):
+    product = document.getElementById("product_name")
+    category = document.getElementById("category")
+    stock = document.getElementById("stock_quantity")
+
+    sku = category.value + "-" + product.value[:3].upper() + "-" + stock.value
+
+    document.getElementById("sku-output").innerText = sku
